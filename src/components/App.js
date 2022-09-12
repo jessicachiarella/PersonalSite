353f14dfe13370fc1586b1c90f6579eefa2f542a
jessicachiarella <jessica.chiarella@gmail.com>
2022-09-12 12:56:43 -0500

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+
+const App = () => {
+    return (
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    )
+}
+export default App;
