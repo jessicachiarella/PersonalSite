23eb01e10e352bb1af28f3855b7d796c41adf45a
jessicachiarella <jessica.chiarella@gmail.com>
2025-04-22 21:13:41 -0500

    fixed build issue

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM, { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("app")!);
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
+const rootElement = document.getElementById('app')
+if(rootElement){
+  const root = createRoot(rootElement)
+  root.render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+} else {
+  console.error("Root element not found")
+}
+
